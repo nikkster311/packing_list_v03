b16b24bf2bdd0a9e33a1f4c512e81aedd258ebad
Nikki <nhermann311@gmail.com>
2020-05-06 16:52:07 -0700

    Updates

---
+++ precache-manifest.2af9ea85429100dd5504d1f5741bb12e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42627293e0f3c40108f2943728fe0a5a",
+    "url": "/packing_list_v03/index.html"
+  },
+  {
+    "revision": "dcd95642f3f69537a3fd",
+    "url": "/packing_list_v03/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b23566ce13e258f6d3f1",
+    "url": "/packing_list_v03/static/js/2.459615db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/packing_list_v03/static/js/2.459615db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd95642f3f69537a3fd",
+    "url": "/packing_list_v03/static/js/main.f4e81ae2.chunk.js"
+  },
+  {
+    "revision": "cd88637aa19e7848e404",
+    "url": "/packing_list_v03/static/js/runtime-main.936fffb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/packing_list_v03/static/media/logo.5d5d9eef.svg"
+  }
+]);
