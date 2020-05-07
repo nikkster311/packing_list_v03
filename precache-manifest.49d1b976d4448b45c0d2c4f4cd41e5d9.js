aa10eff25f70b825d94d1d938626150a353852d5
Nikki <nhermann311@gmail.com>
2020-05-06 18:18:57 -0700

    Updates

---
+++ precache-manifest.49d1b976d4448b45c0d2c4f4cd41e5d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff834b7f0c7bae2e95f20eae3a3b9414",
+    "url": "/packing_list_v03/index.html"
+  },
+  {
+    "revision": "c127296ad432b039b907",
+    "url": "/packing_list_v03/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b23566ce13e258f6d3f1",
+    "url": "/packing_list_v03/static/js/2.459615db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/packing_list_v03/static/js/2.459615db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c127296ad432b039b907",
+    "url": "/packing_list_v03/static/js/main.dfc84f46.chunk.js"
+  },
+  {
+    "revision": "cd88637aa19e7848e404",
+    "url": "/packing_list_v03/static/js/runtime-main.936fffb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/packing_list_v03/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/packing_list_v02/precache-manifest.c479ee6730c1c8c51eb52c73165d0d06.js"
+  "/packing_list_v03/precache-manifest.49d1b976d4448b45c0d2c4f4cd41e5d9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/packing_list_v02/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/packing_list_v03/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
