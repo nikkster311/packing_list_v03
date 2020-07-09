e3527b79ccad7b645934d460e8d7975fec6b3330
Nikki <nhermann311@gmail.com>
2020-07-09 12:10:20 -0700

    Updates

---
+++ precache-manifest.680de46b34dd2d3303407688667754da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c52749ef15943d4384f",
+    "url": "/packing_list_v02/static/css/main.337f6623.chunk.css"
+  },
+  {
+    "revision": "8c52749ef15943d4384f",
+    "url": "/packing_list_v02/static/js/main.8c52749e.chunk.js"
+  },
+  {
+    "revision": "18b553f1a8cf4df6808b",
+    "url": "/packing_list_v02/static/js/runtime~main.18b553f1.js"
+  },
+  {
+    "revision": "32917cb6198359402e0e",
+    "url": "/packing_list_v02/static/js/2.32917cb6.chunk.js"
+  },
+  {
+    "revision": "2a868134e881175e69d3c7a54ae07393",
+    "url": "/packing_list_v02/static/media/alleyway.2a868134.jpg"
+  },
+  {
+    "revision": "072b419bbd497709008bb8aa8ef78bdb",
+    "url": "/packing_list_v02/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/packing_list_v03/precache-manifest.41bcda5ad0f760dace38695db5652fb1.js"
+  "/packing_list_v02/precache-manifest.680de46b34dd2d3303407688667754da.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/packing_list_v03/index.html"), {
+workbox.routing.registerNavigationRoute("/packing_list_v02/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
